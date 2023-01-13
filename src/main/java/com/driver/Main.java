19b070c4551044cd2af76af33025658024c88934
nitish <diluboruah@gmail.com>
2023-01-13 13:39:54 +0530

    sol1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,17 +5,18 @@ public class Main {
       B obj= new B();
       System.out.println(obj.meth());
   }
+    public static class A {
+        String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        @Override
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
 
 
 }
-class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    @Override
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
+
