7c3edb75d0828ec0f560f00cd51569312977c9a2
nitish <diluboruah@gmail.com>
2023-01-13 13:43:58 +0530

    sol2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String []args){
       B obj= new B();
-      System.out.println(obj.meth());
+//      System.out.println(obj.meth());
+      obj.meth();
   }
     public static class A {
         String meth(){
